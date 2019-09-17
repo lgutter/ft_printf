4956889145ec0dd9c1c3e4323ae29a5af549b6fc
Liewe Gutter <lgutter@student.codam.nl>
2019-09-17 10:01:30 +0200

    Fixed norm errors in 4 files
    fixed norm errors in Makefile, ft_printf.c, ft_printf.h & ft_strexpand.c

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: ivan-tey <ivan-tey@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/09/13 14:10:30 by ivan-tey       #+#    #+#                */
-/*   Updated: 2019/09/14 13:53:01 by lgutter       ########   odam.nl         */
+/*   Updated: 2019/09/16 19:17:17 by lgutter       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # include "libft.h"
 # include <stdarg.h>
 
-int		ft_printf(const char * restrict format, ...);
-void    ft_strexpand(char **source, const char *addition);
+int		ft_printf(const char *restrict format, ...);
+void	ft_strexpand(char **source, const char *addition);
 
 #endif
--- ft_strexpand.c
+++ ft_strexpand.c
@@ -6,13 +6,13 @@
 /*   By: lgutter <lgutter@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/09/13 16:04:59 by lgutter        #+#    #+#                */
-/*   Updated: 2019/09/13 17:42:18 by lgutter       ########   odam.nl         */
+/*   Updated: 2019/09/16 17:40:50 by lgutter       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void    ft_strexpand(char **source, const char *addition)
+void	ft_strexpand(char **source, const char *addition)
 {
 	void *temp;
 

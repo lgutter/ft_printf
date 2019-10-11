86937f12357ff2658d75b7c5720e37bc2f35e4c3
Imre van Teylingen <ivan-tey@student.codam.nl>
2019-10-11 14:02:29 +0200

    ft_itoa_base moved to libft

--- ft_printf.h
+++ ft_printf.h
@@ -6,17 +6,16 @@
 /*   By: ivan-tey <ivan-tey@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/09/13 14:10:30 by ivan-tey       #+#    #+#                */
-/*   Updated: 2019/10/11 12:55:58 by ivan-tey      ########   odam.nl         */
+/*   Updated: 2019/10/11 13:59:48 by ivan-tey      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include "libft.h"
+# include "libft/libft.h"
 # include <stdarg.h>
 
 int		ft_printf(const char *restrict format, ...);
-char	*ft_itoa_base(signed long long nb, int base);
 
 #endif

315d643ddaf6c3aafe108f8d1bd18d50a4a833a9
Liewe Gutter <lgutter@student.codam.nl>
2019-10-14 18:19:19 +0200

    added basic tests for dispatcher proof of concept
    added basic tests to cover the proof of concept functionality of the dispatcher.
    for some reason coverage doesn't seem to detect a part of one function, even though I'm 90% sure it is covered...
    wil ltry to fix later.

--- tests/basic_std_arg_test.c
+++ tests/basic_std_arg_test.c
@@ -6,7 +6,7 @@
 /*   By: ivan-tey <ivan-tey@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/09/13 14:28:17 by ivan-tey       #+#    #+#                */
-/*   Updated: 2019/09/14 13:35:10 by ivan-tey      ########   odam.nl         */
+/*   Updated: 2019/10/14 18:00:09 by lgutter       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,30 @@ void redirect_std_out(void)
     cr_redirect_stdout();
 }
 
-Test(redirect, test_outputs, .init = redirect_std_out) {
-    ft_printf("ha%clo%c", 'l', '!');
+Test(redirect, test_char, .init = redirect_std_out) {
+    ft_printf("ha%clo!", 'l');
     fflush(stdout);
 
     cr_assert_stdout_eq_str("hallo!");
+}
+
+Test(redirect, test_string, .init = redirect_std_out) {
+    ft_printf("ha%s!", "llo");
+    fflush(stdout);
+
+    cr_assert_stdout_eq_str("hallo!");
+}
+
+Test(redirect, test_space, .init = redirect_std_out) {
+    ft_printf("ha%  s!", "llo");
+    fflush(stdout);
+
+    cr_assert_stdout_eq_str("ha llo!");
+}
+
+Test(redirect, test_error, .init = redirect_std_out) {
+    ft_printf("ha% b!", "llo");
+    fflush(stdout);
+
+    cr_assert_stdout_eq_str("ha!");
 }

3897d648d989050acaa5ef56f4180d25f0a6ce91
Liewe Gutter <lgutter@student.codam.nl>
2019-10-14 18:17:42 +0200

    modified ft_printf.h and Sourcesfor dispatcher
    updated the ft_printf header file for the dispatcher proof of concept. should possibly be split up in the future to a dispatcher.h
    Also added new functions to Sources file

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: ivan-tey <ivan-tey@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/09/13 14:10:30 by ivan-tey       #+#    #+#                */
-/*   Updated: 2019/09/16 19:17:17 by lgutter       ########   odam.nl         */
+/*   Updated: 2019/10/14 16:49:35 by lgutter       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,23 @@
 # include "libft.h"
 # include <stdarg.h>
 
-int		ft_printf(const char *restrict format, ...);
-void	ft_strexpand(char **source, const char *addition);
+typedef void				(*t_writer)(const char *string);
+
+typedef struct			s_info
+{
+	unsigned short		options;
+	va_list				arguments;
+	size_t				width;
+	size_t				precision;
+	t_writer			writer;
+}						t_info;
+
+typedef int				(*t_formatter)(t_info *info);
+int						ft_printf(const char *restrict format, ...);
+void					ft_strexpand(char **source, const char *addition);
+int						ft_init_info(const char *format, t_info *info);
+t_formatter				ft_dispatcher(char conv_flag);
+int						ft_formatchar(t_info *info);
+int						ft_formatstring(t_info *info);
 
 #endif

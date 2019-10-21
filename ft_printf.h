650a9c3cb346f80861c9062c627418fb22e9981a
Liewe Gutter <lgutter@student.codam.nl>
2019-10-21 17:12:16 +0200

    changed back include of libft in header

--- ft_printf.h
+++ ft_printf.h
@@ -6,14 +6,14 @@
 /*   By: ivan-tey <ivan-tey@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/09/13 14:10:30 by ivan-tey       #+#    #+#                */
-/*   Updated: 2019/10/11 13:59:48 by ivan-tey      ########   odam.nl         */
+/*   Updated: 2019/10/21 17:08:46 by lgutter       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include "libft/libft.h"
+# include "libft.h"
 # include <stdarg.h>
 
 int		ft_printf(const char *restrict format, ...);

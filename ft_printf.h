e89addaac36d3573a36cd15dbe673c41a088e468
Liewe Gutter <lgutter@student.codam.nl>
2019-10-27 18:17:07 +0100

    enums and new functions added to printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: ivan-tey <ivan-tey@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/09/13 14:10:30 by ivan-tey       #+#    #+#                */
-/*   Updated: 2019/10/21 21:59:45 by lgutter       ########   odam.nl         */
+/*   Updated: 2019/10/27 17:37:48 by lgutter       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,26 @@
 # include <stdarg.h>
 
 typedef void				(*t_writer)(const char *string);
+
+/*
+**	enumerations for the flags and modifiers stored in options.
+**	these are written down here as bitshifts, but in essence the exact number
+**	linked to an enum is not relevant, just that we can uniquely store and fetch
+**	any option (flag or modifier)
+*/
+enum					e_options
+{
+	e_L = 1 << 0,
+	e_l = 1 << 1,
+	e_h = 1 << 2,
+	e_ll = 1 << 3,
+	e_hh = 1 << 4,
+	e_hash = 1 << 5,
+	e_zero = 1 << 6,
+	e_space = 1 << 7,
+	e_minus = 1 << 8,
+	e_plus = 1 << 9
+};
 
 typedef struct			s_info
 {
@@ -29,10 +49,11 @@ typedef struct			s_info
 
 typedef int				(*t_formatter)(t_info *info);
 int						ft_printf(const char *restrict format, ...);
-void					ft_strexpand(char **source, const char *addition);
+int						ft_process_conversion(const char *format, t_info *info);
 int						ft_init_info(const char *format, t_info *info);
 t_formatter				ft_dispatcher(char conv_flag);
 int						ft_formatchar(t_info *info);
 int						ft_formatstring(t_info *info);
+int						ft_formatunknown(t_info *info, char c);
 
 #endif

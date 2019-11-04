791635683db12b354f34d2b2f8b245bf8d8765aa
Imre van Teylingen <ivan-tey@student.codam.nl>
2019-11-04 15:18:48 +0100

    even more tests

--- tests/options_test.c
+++ tests/options_test.c
@@ -6,7 +6,7 @@
 /*   By: ivan-tey <ivan-tey@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/10/31 16:37:09 by ivan-tey       #+#    #+#                */
-/*   Updated: 2019/11/04 14:39:00 by ivan-tey      ########   odam.nl         */
+/*   Updated: 2019/11/04 15:18:11 by ivan-tey      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,7 +80,29 @@ Test(init_info_test, flags_lenmod)
 {
 	t_info info;
 
-	char *format = "%ld";
+	char *format = "%0ld";
 	ft_init_info(format, &info);
 	cr_assert_eq(info.lenmod, 0b00000010, "info.lenmod contains %i instead of %i!", info.lenmod, 0b00000010);
+	cr_assert_eq(info.flags, 0b00000010, "info.lenmod contains %i instead of %i!", info.flags, 0b00000010);
 }
+
+Test(init_info_test, width_lenmod_L)
+{
+	t_info info;
+
+	char *format = "%5Ld";
+	ft_init_info(format, &info);
+	cr_assert_eq(info.lenmod, 0b00000001, "info.lenmod contains %i instead of %i!", info.lenmod, 0b00000001);
+	cr_assert_eq(info.width, (size_t)5, "info.lenmod contains %zu instead of %zu!", info.width, (size_t)5);
+}
+
+Test(init_info_test, presicion_lenmod_ll)
+{
+	t_info info;
+
+	char *format = "%.5llf";
+	ft_init_info(format, &info);
+	cr_assert_eq(info.lenmod, 0b00001000, "info.lenmod contains %i instead of %i!", info.lenmod, 0b00001000);
+	cr_assert_eq(info.precision, (size_t)5, "info.lenmod contains %zu instead of %zu!", info.precision, (size_t)5);
+}
+

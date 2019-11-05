651fca25398278c9be126c541a10145614591715
Liewe Gutter <lgutter@student.codam.nl>
2019-11-05 15:46:34 +0100

    fixed includes in 3 files

--- ft_printf.h
+++ ft_printf.h
@@ -6,14 +6,14 @@
 /*   By: ivan-tey <ivan-tey@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/09/13 14:10:30 by ivan-tey       #+#    #+#                */
-/*   Updated: 2019/11/04 17:05:54 by lgutter       ########   odam.nl         */
+/*   Updated: 2019/11/05 15:25:19 by lgutter       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include "libft/libft.h"
+# include "libft.h"
 # include <stdarg.h>
 
 typedef void				(*t_writer)(const char *string, size_t len);

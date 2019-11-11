b2157f2bf1458fb69567c4e98bd42d072988a252
Liewe Gutter <lgutter@student.codam.nl>
2019-11-11 18:52:22 +0100

    changed all relevant functions for new structure
    all relevant functions now work with the new info struct and writer function pointer.

--- ft_formatchar.c
+++ ft_formatchar.c
@@ -6,27 +6,27 @@
 /*   By: lgutter <lgutter@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/10/14 16:40:16 by lgutter        #+#    #+#                */
-/*   Updated: 2019/11/11 16:54:23 by lgutter       ########   odam.nl         */
+/*   Updated: 2019/11/11 18:35:36 by lgutter       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int		ft_formatchar(t_info *info, t_convinfo *convinfo)
+int		ft_formatchar(t_info *info)
 {
 	char c;
 
-	convinfo->type = 'c';
+	info->conv = 'c';
 	c = va_arg(info->arguments, int);
 	if ((info->flags & e_minus) != 0)
 	{
-		info->writer(&c, 1);
+		info->writer(info->target, &c, 1);
 		ft_check_width(info, 1);
 	}
 	else
 	{
 		ft_check_width(info, 1);
-		info->writer(&c, 1);
+		info->writer(info->target, &c, 1);
 	}
 	return (0);
 }
--- ft_process_conversion.c
+++ ft_process_conversion.c
@@ -6,7 +6,7 @@
 /*   By: lgutter <lgutter@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/10/25 15:46:25 by lgutter        #+#    #+#                */
-/*   Updated: 2019/11/06 18:33:53 by ivan-tey      ########   odam.nl         */
+/*   Updated: 2019/11/11 18:33:37 by lgutter       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,12 @@ int		ft_process_conversion(const char *format, t_info *info)
 {
 	int			i;
 	t_formatter	formatter;
-	t_convinfo	convinfo;
 
 	i = ft_init_info(format, info);
 	formatter = ft_dispatcher(format[i]);
 	if (formatter != NULL)
 	{
-		formatter(info, &convinfo);
+		formatter(info);
 	}
 	else
 	{

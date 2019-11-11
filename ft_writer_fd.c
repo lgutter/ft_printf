7322cfecbcbf8943b49084a98e754adc40a37ced
Liewe Gutter <lgutter@student.codam.nl>
2019-11-11 18:50:33 +0100

    changed ft_writer to ft_writer_fd
    ft_writer is now ft_writer_fd.
    only difference is that it works with the new structure of printf,
    allowing it to write to any fd provided through a void *.

--- ft_writer_fd.c
+++ ft_writer_fd.c
@@ -1,25 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   ft_writer.c                                        :+:    :+:            */
+/*   ft_writer_fd.c                                     :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: lgutter <lgutter@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/04 17:03:28 by lgutter        #+#    #+#                */
-/*   Updated: 2019/11/04 17:05:13 by lgutter       ########   odam.nl         */
+/*   Updated: 2019/11/11 18:38:57 by lgutter       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	ft_writer(const char *string, size_t len)
+void	ft_writer_fd(void *target, const char *string, size_t len)
 {
+	int fd;
+
+	fd = *((int *)target);
 	if (len > 0)
 	{
-		write(1, string, len);
+		write(fd, string, len);
 	}
 	else
 	{
-		write(1, string, ft_strlen(string));
+		write(fd, string, ft_strlen(string));
 	}
 }

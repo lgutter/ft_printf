d6aaca747db3e775dad471469d0960ef2c6224f9
lgutter <lgutter@student.codam.nl>
2019-11-12 20:35:18 +0100

    added fd to int tests
    not sure how this worked before,
    but there was no pointer to fd in these tests.
    added to all so tests are functional.

--- tests/int_tests.c
+++ tests/int_tests.c
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+ /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
 /*   int_tests.c                                        :+:    :+:            */
@@ -16,7 +16,7 @@
 
 void redirect_std_out_format_int(void)
 {
-    cr_redirect_stdout();
+	cr_redirect_stdout();
 }
 
 static void simplewriter(void *target, const char *str, unsigned long len)
@@ -36,101 +36,119 @@ static void simplewriter(void *target, const char *str, unsigned long len)
 
 void init_va_list(t_info *info, ...)
 {
-    va_start(info->arguments, info);
+	va_start(info->arguments, info);
 }
 
 Test(test_format_int, int_simple_nb, .init = redirect_std_out_format_int)
 {
-    t_info info;
-    int d;
+	t_info info;
+	int d;
+	int fd;
 
-    d = 24;
-    init_va_list(&info, d);
-    info.writer = &simplewriter;
-    info.flags = 0;
-    info.width = 0;
-    ft_formatint(&info);
-    fflush(stdout);
+	fd = stdout;
+	d = 24;
+	init_va_list(&info, d);
+	info.writer = &simplewriter;
+	info.flags = 0;
+	info.width = 0;
+	info.target = &fd;
+	ft_formatint(&info);
+	fflush(stdout);
 
-    cr_assert_stdout_eq_str("24");
+	cr_assert_stdout_eq_str("24");
 }
 
 Test(test_format_int, int_simple_minus_nb, .init = redirect_std_out_format_int)
 {
-    t_info info;
-    int d;
+	t_info info;
+	int d;
+	int fd;
 
-    d = -24;
-    init_va_list(&info, d);
-    info.writer = &simplewriter;
-    info.flags = 0;
-    info.width = 0;
-    ft_formatint(&info);
-    fflush(stdout);
+	fd = stdout;
+	d = -24;
+	init_va_list(&info, d);
+	info.writer = &simplewriter;
+	info.flags = 0;
+	info.width = 0;
+	info.target = &fd;
+	ft_formatint(&info);
+	fflush(stdout);
 
-    cr_assert_stdout_eq_str("-24");
+	cr_assert_stdout_eq_str("-24");
 }
 
 Test(test_format_int, int_width_nb, .init = redirect_std_out_format_int)
 {
-    t_info info;
-    int d;
+	t_info info;
+	int d;
+	int fd;
 
-    d = 24;
-    init_va_list(&info, d);
-    info.writer = &simplewriter;
-    info.flags = 0;
-    info.width = 5;
-    ft_formatint(&info);
-    fflush(stdout);
+	fd = stdout;
+	d = 24;
+	init_va_list(&info, d);
+	info.writer = &simplewriter;
+	info.flags = 0;
+	info.width = 5;
+	info.target = &fd;
+	ft_formatint(&info);
+	fflush(stdout);
 
-    cr_assert_stdout_eq_str("   24");
+	cr_assert_stdout_eq_str("   24");
 }
 
 Test(test_format_int, int_minus_flag_nb, .init = redirect_std_out_format_int)
 {
-    t_info info;
-    int d;
+	t_info info;
+	int d;
+	int fd;
 
-    d = 24;
-    init_va_list(&info, d);
-    info.writer = &simplewriter;
-    info.flags = e_minus;
-    info.width = 5;
-    ft_formatint(&info);
-    fflush(stdout);
+	fd = stdout;
+	d = 24;
+	init_va_list(&info, d);
+	info.writer = &simplewriter;
+	info.flags = e_minus;
+	info.width = 5;
+	info.target = &fd;
+	ft_formatint(&info);
+	fflush(stdout);
 
-    cr_assert_stdout_eq_str("24   ");
+	cr_assert_stdout_eq_str("24   ");
 }
 
 Test(test_format_int, int_zero_flag_nb, .init = redirect_std_out_format_int)
 {
-    t_info info;
-    int d;
+	t_info info;
+	int d;
+	int fd;
 
-    d = 24;
-    init_va_list(&info, d);
-    info.writer = &simplewriter;
-    info.flags = e_zero;
-    info.width = 5;
-    ft_formatint(&info);
-    fflush(stdout);
+	fd = stdout;
+	d = 24;
+	init_va_list(&info, d);
+	info.writer = &simplewriter;
+	info.flags = e_zero;
+	info.width = 5;
+	info.target = &fd;
+	ft_formatint(&info);
+	fflush(stdout);
 
-    cr_assert_stdout_eq_str("00024");
+	cr_assert_stdout_eq_str("00024");
 }
 
 Test(test_format_int, int_zero_minus_nb, .init = redirect_std_out_format_int)
 {
-    t_info info;
-    int d;
+	t_info info;
+	int d;
+	int fd;
 
-    d = -24;
-    init_va_list(&info, d);
-    info.writer = &simplewriter;
-    info.flags = e_zero;
-    info.width = 5;
-    ft_formatint(&info);
-    fflush(stdout);
+	fd = stdout;
+	d = -24;
+	init_va_list(&info, d);
+	info.writer = &simplewriter;
+	info.flags = e_zero;
+	info.width = 5;
+	info.target = &fd;
+	ft_formatint(&info);
+	fflush(stdout);
 
-    cr_assert_stdout_eq_str("-0024");
+	cr_assert_stdout_eq_str("-0024");
 }

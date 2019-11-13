d9ffd1f324af7ac3d052bcbafdf9e901690c08b9
Liewe Gutter <lgutter@student.codam.nl>
2019-11-13 15:23:26 +0100

    made ft_write_flags modify len properly.

--- ft_write_flags.c
+++ ft_write_flags.c
@@ -6,7 +6,7 @@
 /*   By: lgutter <lgutter@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/13 13:52:57 by lgutter        #+#    #+#                */
-/*   Updated: 2019/11/13 14:02:42 by lgutter       ########   odam.nl         */
+/*   Updated: 2019/11/13 15:21:50 by lgutter       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,14 @@ void	ft_write_flags(t_info *info)
 	if ((info->conv == 'd' || info->conv == 'i') && info->sign >= 0)
 	{
 		if (info->flags & e_plus)
+		{
+			info->len++;
 			info->writer(info->target, "+", 1);
+		}
 		else if (info->flags & e_space)
+		{
+			info->len++;
 			info->writer(info->target, " ", 1);
+		}
 	}
 }

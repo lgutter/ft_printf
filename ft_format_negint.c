6e99d9d2f23132d18728e7e0d3689de167c4886c
Liewe Gutter <lgutter@student.codam.nl>
2019-11-13 14:48:56 +0100

    restructured formatint funcs for lenmod
    ft_formatint is now updated for the new workflow with ft_formatdecimal,
    This is so we can integrate lenmod. ft_handle_negint is now ft_format_negint.
    That function is also updated, it is now an alternative to ft_formatint instead of a part of.

--- ft_format_negint.c
+++ ft_format_negint.c
@@ -1,28 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   ft_handle_negint.c                                 :+:    :+:            */
+/*   ft_format_negint.c                                 :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: ivan-tey <ivan-tey@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/11 12:16:15 by ivan-tey       #+#    #+#                */
-/*   Updated: 2019/11/12 14:38:47 by ivan-tey      ########   odam.nl         */
+/*   Updated: 2019/11/13 14:24:54 by lgutter       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int			ft_handle_negint(int n, t_info *info)
+int		ft_format_negint(int n, t_info *info)
 {
-	if (n < 0)
+	char *nb;
+
+	info->sign = -1;
+	if ((info->flags & e_zero) != 0)
 	{
-		info->sign = -1;
-		if ((info->flags & e_zero) != 0)
-		{
-			info->writer(info->target, "-", 1);
-			n = n * -1;
-			info->len = ft_nbrlenbase(n, 10) + 1;
-		}
+		info->writer(info->target, "-", 1);
+		info->len = ft_nbrlenbase(n, 10);
+		n = n * -1;
 	}
-	return (n);
+	else
+		info->len = ft_nbrlenbase(n, 10);
+	nb = ft_itoa_base(n, 10);
+	if (nb == NULL)
+		return (-1);
+	if ((info->flags & e_minus) != 0)
+	{
+		info->writer(info->target, nb, 0);
+		ft_check_width(info, info->len);
+	}
+	else
+	{
+		ft_check_width(info, info->len);
+		info->writer(info->target, nb, 0);
+	}
+	return (0);
 }
--- ft_formatint.c
+++ ft_formatint.c
@@ -6,7 +6,7 @@
 /*   By: ivan-tey <ivan-tey@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/11 11:48:34 by ivan-tey       #+#    #+#                */
-/*   Updated: 2019/11/12 10:59:18 by ivan-tey      ########   odam.nl         */
+/*   Updated: 2019/11/13 14:24:10 by lgutter       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,26 @@ int			ft_formatint(t_info *info)
 	int		n;
 	char	*nb;
 
-	info->conv = 'd';
 	n = va_arg(info->arguments, int);
 	info->len = ft_nbrlenbase(n, 10);
-	n = ft_handle_negint(n, info);
-	nb = ft_itoa(n);
+	if (n < 0)
+		return (ft_format_negint(n, info));
+	info->sign = 1;
+	nb = ft_itoa_base(n, 10);
 	if ((info->flags & e_minus) != 0)
 	{
+		ft_write_flags(info);
 		info->writer(info->target, nb, 0);
 		ft_check_width(info, info->len);
-		return (0);
 	}
-	ft_check_width(info, info->len);
-	info->writer(info->target, nb, 0);
+	else
+	{
+		if ((info->flags & e_zero) != 0)
+			ft_write_flags(info);
+		ft_check_width(info, info->len);
+		if ((info->flags & e_zero) == 0)
+			ft_write_flags(info);
+		info->writer(info->target, nb, 0);
+	}
 	return (0);
 }

f9bad279f70c9508dfb0e10f8d08c92f05e19204
Imre van Teylingen <ivan-tey@student.codam.nl>
2019-11-15 10:34:17 +0100

    started testing, wrong

--- tests/pointer_tests.c
+++ tests/pointer_tests.c
@@ -6,13 +6,13 @@
 /*   By: ivan-tey <ivan-tey@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/14 17:00:19 by ivan-tey       #+#    #+#                */
-/*   Updated: 2019/11/14 17:07:30 by ivan-tey      ########   odam.nl         */
+/*   Updated: 2019/11/15 10:31:08 by ivan-tey      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <criterion/criterion.h>
 #include <criterion/redirect.h>
-#include "ft_printf.h"
+#include "../ft_printf.h"
 
 void redirect_std_out_format_pointer(void)
 {
@@ -37,4 +37,26 @@ static void simplewriter(void *target, const char *str, unsigned long len)
 void init_va_list_pointer(t_info *info, ...)
 {
 	va_start(info->arguments, info);
+}
+
+Test(test_format_pointer, pointer_simple, .init = redirect_std_out_format_pointer)
+{
+	t_info info;
+	int *p;
+	int a;
+	int fd;
+
+	fd = 1;
+	a = 24;
+	p = &a;
+	init_va_list_pointer(&info, p);
+	info.writer = &simplewriter;
+	info.flags = 0;
+	info.width = 0;
+	info.target = &fd;
+	info.conv = 'p';
+	ft_formatint(&info);
+	fflush(stdout);
+
+	cr_assert_stdout_eq_str("0x7ffeea58f9b8");
 }

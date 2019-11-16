5d10dc1e4d0970c2c76729caa1835f4eb9a8f00e
Liewe Gutter <lgutter@student.codam.nl>
2019-11-16 18:45:48 +0100

    implemented asterisk in width
    implented the asterisk functionality for width and added tests for it.

--- tests/check_width_tests.c
+++ tests/check_width_tests.c
@@ -6,92 +6,140 @@
 /*   By: ivan-tey <ivan-tey@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/07 11:32:15 by ivan-tey       #+#    #+#                */
-/*   Updated: 2019/11/10 17:52:49 by lgutter       ########   odam.nl         */
+/*   Updated: 2019/11/16 18:43:07 by lgutter       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <criterion/criterion.h>
 #include <criterion/redirect.h>
 #include "ft_printf.h"
+#include <stdio.h>
 
-void redirect_stdout(void)
+static void redirect_stdout(void)
 {
     cr_redirect_stdout();
 }
 
-Test(redirect, width_char_none, .init = redirect_stdout) {
+Test(test_width, width_char_none, .init = redirect_stdout) {
     ft_printf("ha%clo!", 'l');
     fflush(stdout);
 
     cr_assert_stdout_eq_str("hallo!");
 }
 
-Test(redirect, width_char_ten, .init = redirect_stdout) {
+Test(test_width, width_char_ten, .init = redirect_stdout) {
     ft_printf("ha%10clo!", 'l');
     fflush(stdout);
 
     cr_assert_stdout_eq_str("ha         llo!");
 }
 
-Test(redirect, width_char_fourty, .init = redirect_stdout) {
+Test(test_width, width_char_fourty, .init = redirect_stdout) {
     ft_printf("ha%40clo!", 'l');
     fflush(stdout);
 
     cr_assert_stdout_eq_str("ha                                       llo!");
 }
 
-Test(redirect, width_char_zero_ten, .init = redirect_stdout) {
+Test(test_width, width_char_zero_ten, .init = redirect_stdout) {
     ft_printf("ha%010clo!", 'l');
     fflush(stdout);
 
     cr_assert_stdout_eq_str("ha000000000llo!");
 }
 
-Test(redirect, width_char_zero_fourty, .init = redirect_stdout) {
+Test(test_width, width_char_zero_fourty, .init = redirect_stdout) {
     ft_printf("ha%040clo!", 'l');
     fflush(stdout);
 
     cr_assert_stdout_eq_str("ha000000000000000000000000000000000000000llo!");
 }
 
-Test(redirect, width_char_minus_ten, .init = redirect_stdout) {
+Test(test_width, width_char_minus_ten, .init = redirect_stdout) {
     ft_printf("ha%-10clo!", 'l');
     fflush(stdout);
 
     cr_assert_stdout_eq_str("hal         lo!");
 }
 
-Test(redirect, width_string_none, .init = redirect_stdout) {
+Test(test_width, width_string_none, .init = redirect_stdout) {
     ft_printf("ha%so!", "ll");
     fflush(stdout);
 
     cr_assert_stdout_eq_str("hallo!");
 }
 
-Test(redirect, width_string_ten, .init = redirect_stdout) {
+Test(test_width, width_string_ten, .init = redirect_stdout) {
     ft_printf("ha%10so!", "ll");
     fflush(stdout);
 
     cr_assert_stdout_eq_str("ha        llo!");
 }
 
-Test(redirect, width_string_zero_ten, .init = redirect_stdout) {
+Test(test_width, width_string_zero_ten, .init = redirect_stdout) {
     ft_printf("ha%010so!", "ll");
     fflush(stdout);
 
     cr_assert_stdout_eq_str("ha00000000llo!");
 }
 
-Test(redirect, width_string_minus_ten, .init = redirect_stdout) {
+Test(test_width, width_string_minus_ten, .init = redirect_stdout) {
     ft_printf("ha%-10so!", "ll");
     fflush(stdout);
 
     cr_assert_stdout_eq_str("hall        o!");
 }
 
-Test(redirect, width_string_zer_minus_ten, .init = redirect_stdout) {
+Test(test_width, width_string_zer_minus_ten, .init = redirect_stdout) {
     ft_printf("ha%0-10so!", "ll");
     fflush(stdout);
 
     cr_assert_stdout_eq_str("hall        o!");
+}
+
+Test(test_width, zero_minus_asterisk_string, .init = redirect_stdout)
+{
+	char *str = "hoi";
+	char *result = NULL;
+	ft_printf("%0-*s", 5, str);
+	fflush(stdout);
+
+	asprintf(&result, "%-*s", 5, str);
+	cr_assert_stdout_eq_str(result);
+}
+
+Test(test_width, zero_minus_asterisk_int, .init = redirect_stdout)
+{
+	int d;
+	char *result = NULL;
+	d = 424242;
+	ft_printf("%0-*i", 12, d);
+	fflush(stdout);
+
+	asprintf(&result, "%-*i", 12, d);
+	cr_assert_stdout_eq_str(result);
+}
+
+Test(test_width, double_minus_asterisk_int, .init = redirect_stdout)
+{
+	int d;
+	char *result = NULL;
+	d = 424242;
+	ft_printf("%--*i", 12, d);
+	fflush(stdout);
+
+	asprintf(&result, "%--*i", 12, d);
+	cr_assert_stdout_eq_str(result);
+}
+
+Test(test_width, double_minus_twelve_int, .init = redirect_stdout)
+{
+	int d;
+	char *result = NULL;
+	d = 424242;
+	ft_printf("%--12i", d);
+	fflush(stdout);
+
+	asprintf(&result, "%--12i", d);
+	cr_assert_stdout_eq_str(result);
 }

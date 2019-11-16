9ff67eb0f370747124551bd76ee6644c9f2b343a
Liewe Gutter <lgutter@student.codam.nl>
2019-11-16 15:46:06 +0100

    stash pop

--- ft_formatbyteint.c
+++ ft_formatbyteint.c
@@ -6,15 +6,66 @@
 /*   By: lgutter <lgutter@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/13 14:34:50 by lgutter        #+#    #+#                */
-/*   Updated: 2019/11/13 14:39:14 by lgutter       ########   odam.nl         */
+/*   Updated: 2019/11/16 14:17:12 by lgutter       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+static int	ft_format_negbyteint(char n, t_info *info)
+{
+	char *nb;
+
+	info->sign = -1;
+	if ((info->flags & e_zero) != 0)
+	{
+		info->writer(info->target, "-", 1);
+		info->len = ft_nbrlenbase((long long)n, 10);
+		n = n * -1;
+	}
+	else
+		info->len = ft_nbrlenbase((long long)n, 10);
+	nb = ft_itoa_base((long long)n, 10);
+	if (nb == NULL)
+		return (-1);
+	if ((info->flags & e_minus) != 0)
+	{
+		info->writer(info->target, nb, 0);
+		ft_check_width(info, info->len);
+	}
+	else
+	{
+		ft_check_width(info, info->len);
+		info->writer(info->target, nb, 0);
+	}
+	return (0);
+}
+
 int			ft_formatbyteint(t_info *info)
 {
-	if (info)
-		return (0);
+	char	n;
+	char	*nb;
+
+	n = va_arg(info->arguments, int);
+	info->len = ft_nbrlenbase((long long)n, 10);
+	if (n < 0)
+		return (ft_format_negbyteint(n, info));
+	info->sign = 1;
+	nb = ft_itoa_base((long long)n, 10);
+	if ((info->flags & e_minus) != 0)
+	{
+		ft_write_flags(info);
+		info->writer(info->target, nb, 0);
+		ft_check_width(info, info->len);
+	}
+	else
+	{
+		if ((info->flags & e_zero) != 0)
+			ft_write_flags(info);
+		ft_check_width(info, info->len);
+		if ((info->flags & e_zero) == 0)
+			ft_write_flags(info);
+		info->writer(info->target, nb, 0);
+	}
 	return (0);
 }
--- libft/ft_strcharjoin.c
+++ libft/ft_strcharjoin.c
@@ -6,7 +6,7 @@
 /*   By: lgutter <lgutter@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/10/27 12:13:40 by lgutter        #+#    #+#                */
-/*   Updated: 2019/10/27 18:06:19 by lgutter       ########   odam.nl         */
+/*   Updated: 2019/11/16 15:35:45 by lgutter       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 char	*ft_strcharjoin(char *str, char c)
 {
-	char	*strnew;
+	char		*strnew;
 	size_t		i;
 
 	i = 0;

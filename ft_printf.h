d5213bda70cede0a245eb220170669463f826173
Liewe Gutter <lgutter@student.codam.nl>
2019-11-18 19:59:10 +0100

    rebuild int formatter
    rebuild int formatting so it can use one function (formatlonglong)
    deleted obsolete functions.

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: ivan-tey <ivan-tey@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/09/13 14:10:30 by ivan-tey       #+#    #+#                */
-/*   Updated: 2019/11/18 15:32:44 by lgutter       ########   odam.nl         */
+/*   Updated: 2019/11/18 19:58:16 by lgutter       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,12 +81,8 @@ char					*ft_precision_int(t_info *info, char *number);
 int						ft_formatchar(t_info *info);
 int						ft_formatstring(t_info *info);
 int						ft_formatunknown(t_info *info, char c);
-int						ft_formatint(t_info *info);
 int						ft_formatdecimal(t_info *info);
-int						ft_formatbyteint(t_info *info);
-int						ft_formatshort(t_info *info);
-int						ft_formatlong(t_info *info);
-int						ft_formatlonglong(t_info *info);
+int						ft_formatlonglong(long long n, t_info *info);
 int						ft_formatoctal(t_info *info);
 int						ft_formatpointer(t_info *info);
 

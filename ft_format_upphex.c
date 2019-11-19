3c5fc2e1aceef34756f9f56cd9c657d66356ffdf
Imre van Teylingen <ivan-tey@student.codam.nl>
2019-11-19 15:32:38 +0100

    lenmod for upper case hex

---
+++ ft_format_upphex.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_format_upphex.c                                 :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: ivan-tey <ivan-tey@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/11/19 15:25:06 by ivan-tey       #+#    #+#                */
+/*   Updated: 2019/11/19 15:25:31 by ivan-tey      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int			ft_format_upphex(t_info *info)
+{
+	unsigned long long n;
+
+	info->conv = 'x';
+	if ((info->lenmod & e_hh) != 0)
+	{
+		n = (unsigned char)va_arg(info->arguments, int);
+		return (ft_formatullupphex(n, info));
+	}
+	if ((info->lenmod & e_h) != 0)
+	{
+		n = (short)va_arg(info->arguments, int);
+		return (ft_formatullupphex(n, info));
+	}
+	if ((info->lenmod & e_ll) != 0 || (info->lenmod & e_l) != 0)
+	{
+		n = va_arg(info->arguments, long long);
+		return (ft_formatullupphex(n, info));
+	}
+	n = (long long)va_arg(info->arguments, int);
+	return (ft_formatullupphex(n, info));
+}
--- ft_printf.h
+++ ft_printf.h
@@ -6,14 +6,14 @@
 /*   By: ivan-tey <ivan-tey@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/09/13 14:10:30 by ivan-tey       #+#    #+#                */
-/*   Updated: 2019/11/19 15:22:06 by ivan-tey      ########   odam.nl         */
+/*   Updated: 2019/11/19 15:26:28 by ivan-tey      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include "libft.h"
+# include "libft/libft.h"
 # include <stdarg.h>
 
 typedef void			(*t_writer)\
@@ -92,6 +92,8 @@ int						ft_formatoctal(t_info *info);
 int						ft_formatpointer(t_info *info);
 int						ft_formatupphex(t_info *info);
 int						ft_formatlowhex(t_info *info);
+int						ft_format_upphex(t_info *info);
+int						ft_formatullupphex(unsigned long long n, t_info *info);
 
 int						ft_find_flags(const char *format, t_info *info, int i);
 int						ft_find_width(const char *format, t_info *info, int i);

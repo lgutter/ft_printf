8fba6f7bda0ab4c5407eda7fe21e3eb274f0d8e1
Imre van Teylingen <ivan-tey@student.codam.nl>
2019-11-19 15:49:47 +0100

    small change

--- ft_formatulllowhex.c
+++ ft_formatulllowhex.c
@@ -6,7 +6,7 @@
 /*   By: ivan-tey <ivan-tey@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/18 14:50:31 by ivan-tey       #+#    #+#                */
-/*   Updated: 2019/11/19 12:15:13 by ivan-tey      ########   odam.nl         */
+/*   Updated: 2019/11/19 15:49:31 by ivan-tey      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ int			ft_formatulllowhex(unsigned long long n, t_info *info)
 	info->sign = 1;
 	info->len = ft_nbrlenbase_ull(n, 16);
 	nb = ft_ulltoa_base_low(n, 16);
-	nb = ft_precision_int(info, nb);
 	if (nb == NULL)
 		return (-1);
 	if ((info->flags & e_hash) != 0 && n == 0)

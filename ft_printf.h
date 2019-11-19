37e546fc42be5f7a98d0b7d8bade82c03cc6969c
Liewe Gutter <lgutter@student.codam.nl>
2019-11-19 10:30:04 +0100

    added formatunsigned functions

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: ivan-tey <ivan-tey@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/09/13 14:10:30 by ivan-tey       #+#    #+#                */
-/*   Updated: 2019/11/18 18:13:07 by ivan-tey      ########   odam.nl         */
+/*   Updated: 2019/11/19 10:02:05 by lgutter       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,6 +77,7 @@ t_formatter				ft_dispatcher(char conv_flag);
 void					ft_writer_fd\
 						(void *target, const char *string, size_t len);
 char					*ft_precision_int(t_info *info, char *number);
+int						ft_formatunsigneddecimal(t_info *info);
 
 int						ft_formatchar(t_info *info);
 int						ft_formatstring(t_info *info);
@@ -87,6 +88,8 @@ int						ft_formatbyteint(t_info *info);
 int						ft_formatshort(t_info *info);
 int						ft_formatlong(t_info *info);
 int						ft_formatlonglong(t_info *info);
+int						ft_formatunsignedlonglong(\
+						unsigned long long n, t_info *info);
 int						ft_formatoctal(t_info *info);
 int						ft_formatpointer(t_info *info);
 int						ft_formatupphex(t_info *info);

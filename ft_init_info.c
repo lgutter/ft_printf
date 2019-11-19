8021e235e7fba6567ac63f6f1412857ee75c7060
Liewe Gutter <lgutter@student.codam.nl>
2019-11-19 16:45:24 +0100

    initialized precfound

--- ft_init_info.c
+++ ft_init_info.c
@@ -6,7 +6,7 @@
 /*   By: lgutter <lgutter@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/10/14 14:50:09 by lgutter        #+#    #+#                */
-/*   Updated: 2019/11/04 10:42:16 by ivan-tey      ########   odam.nl         */
+/*   Updated: 2019/11/19 16:19:42 by lgutter       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ int	ft_init_info(const char *format, t_info *info)
 	info->width = 0;
 	info->precision = 0;
 	info->lenmod = 0;
+	info->precfound = -1;
 	i = ft_find_flags(format, info, i);
 	i = ft_find_width(format, info, i);
 	i = ft_find_precision(format, info, i);

d7a5df1c0e40fb4dbe978ebdb73c044db8527bd5
imrew-vantey <47398943+imrew-vantey@users.noreply.github.com>
2019-11-20 16:51:29 +0100

    Feature/prec octal hex (#85)
    * changed casts and octal to ulltoa
    
    * precision for 'o' 'x' and 'X' with tests

---
+++ tests/octal_precision_tests.c
@@ -0,0 +1,248 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   octal_precision_tests.c                            :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: ivan-tey <ivan-tey@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/11/20 13:09:17 by ivan-tey       #+#    #+#                */
+/*   Updated: 2019/11/20 15:31:19 by ivan-tey      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+#include "ft_printf.h"
+
+static void	redirect_std_out_octal_prec(void)
+{
+	cr_redirect_stdout();
+}
+
+Test(test_printf_precision_octal, octal_zero_23_zero_precision_d, .init = redirect_std_out_octal_prec)
+{
+	int d;
+	char *result = NULL;
+	d = 23;
+	ft_printf("%05.0o", d);
+	fflush(stdout);
+
+	asprintf(&result, "%05.0o", d);
+	cr_assert_stdout_eq_str(result);
+}
+
+Test(test_printf_precision_octal, octal_zero_zero_zero_precision_d, .init = redirect_std_out_octal_prec)
+{
+	int d;
+	char *result = NULL;
+	d = 0;
+	ft_printf("|%0.0o|", d);
+	fflush(stdout);
+
+	asprintf(&result, "|%0.0o|", d);
+	cr_assert_stdout_eq_str(result);
+}
+
+Test(test_printf_precision_octal, octal_zero_zero_five_precision_d, .init = redirect_std_out_octal_prec)
+{
+	int d;
+	char *result = NULL;
+	d = 0;
+	ft_printf("%05.5o", d);
+	fflush(stdout);
+
+	asprintf(&result, "%05.5o", d);
+	cr_assert_stdout_eq_str(result);
+}
+
+Test(test_printf_precision_octal, octal_zero_zero_empty_precision_d, .init = redirect_std_out_octal_prec)
+{
+	int d;
+	char *result = NULL;
+	d = 0;
+	ft_printf("|%0.o|", d);
+	fflush(stdout);
+
+	asprintf(&result, "|%0.o|", d);
+	cr_assert_stdout_eq_str(result);
+}
+
+Test(test_printf_precision_octal, octal_minus_2_width_zero_empty_precision_d, .init = redirect_std_out_octal_prec)
+{
+	int d;
+	char *result = NULL;
+	d = 0;
+	ft_printf("%-2.o", d);
+	fflush(stdout);
+
+	asprintf(&result, "%-2.o", d);
+	cr_assert_stdout_eq_str(result);
+}
+
+Test(test_printf_precision_octal, octal_zero_4242_five_precision_d, .init = redirect_std_out_octal_prec)
+{
+	int d;
+	char *result = NULL;
+	d = 4242;
+	ft_printf("%0.5o", d);
+	fflush(stdout);
+
+	asprintf(&result, "%0.5o", d);
+	cr_assert_stdout_eq_str(result);
+}
+
+Test(test_printf_precision_octal, octal_4242_five_precision_d, .init = redirect_std_out_octal_prec)
+{
+	int d;
+	char *result = NULL;
+	d = 4242;
+	ft_printf("%3.5o", d);
+	fflush(stdout);
+
+	asprintf(&result, "%3.5o", d);
+	cr_assert_stdout_eq_str(result);
+}
+
+Test(test_printf_precision_octal, octal_5_width_4242_3_precision_d, .init = redirect_std_out_octal_prec)
+{
+	int d;
+	char *result = NULL;
+	d = 4242;
+	ft_printf("%3.5o", d);
+	fflush(stdout);
+
+	asprintf(&result, "%3.5o", d);
+	cr_assert_stdout_eq_str(result);
+}
+
+Test(test_printf_precision_octal, octal_minus_5_width_4242_3_precision_d, .init = redirect_std_out_octal_prec)
+{
+	int d;
+	char *result = NULL;
+	d = 4242;
+	ft_printf("%-3.5o", d);
+	fflush(stdout);
+
+	asprintf(&result, "%-3.5o", d);
+	cr_assert_stdout_eq_str(result);
+}
+
+Test(test_printf_precision_octal, octal_minus_5_width_4242_3_precision_hh_d, .init = redirect_std_out_octal_prec)
+{
+	char d;
+	char *result = NULL;
+	d = (char)4242;
+	ft_printf("%-3.5hho", d);
+	fflush(stdout);
+
+	asprintf(&result, "%-3.5hho", d);
+	cr_assert_stdout_eq_str(result);
+}
+
+Test(test_printf_precision_octal, octal_minus_5_width_4242_3_precision_h_d, .init = redirect_std_out_octal_prec)
+{
+	short d;
+	char *result = NULL;
+	d = 4242;
+	ft_printf("%-3.5ho", d);
+	fflush(stdout);
+
+	asprintf(&result, "%-3.5ho", d);
+	cr_assert_stdout_eq_str(result);
+}
+
+Test(test_printf_precision_octal, octal_minus_5_width_0_3_precision_h_d, .init = redirect_std_out_octal_prec)
+{
+	short d;
+	char *result = NULL;
+	d = 0;
+	ft_printf("%-3.5ho", d);
+	fflush(stdout);
+
+	asprintf(&result, "%-3.5ho", d);
+	cr_assert_stdout_eq_str(result);
+}
+
+Test(test_printf_precision_octal, octal_minus_5_width_4242_12_precision_l_d, .init = redirect_std_out_octal_prec)
+{
+	long d;
+	char *result = NULL;
+	d = 4242;
+	ft_printf("%-3.12lo", d);
+	fflush(stdout);
+
+	asprintf(&result, "%-3.12lo", d);
+	cr_assert_stdout_eq_str(result);
+}
+
+Test(test_printf_precision_octal, octal_minus_3_width_0_3_precision_l_d, .init = redirect_std_out_octal_prec)
+{
+	long d;
+	char *result = NULL;
+	d = 0;
+	ft_printf("%-3.3lo", d);
+	fflush(stdout);
+
+	asprintf(&result, "%-3.3lo", d);
+	cr_assert_stdout_eq_str(result);
+}
+
+Test(test_printf_precision_octal, octal_minus_3_width_MAX_5_precision_l_d, .init = redirect_std_out_octal_prec)
+{
+	long d;
+	char *result = NULL;
+	d = 9223372036854775807;
+	ft_printf("%-3.5lo", d);
+	fflush(stdout);
+
+	asprintf(&result, "%-3.5lo", d);
+	cr_assert_stdout_eq_str(result);
+}
+
+Test(test_printf_precision_octal, octal_minus_5_width_MAX_32_precision_l_d, .init = redirect_std_out_octal_prec)
+{
+	long d;
+	char *result = NULL;
+	d = 9223372036854775807;
+	ft_printf("%-5.32lo", d);
+	fflush(stdout);
+
+	asprintf(&result, "%-5.32lo", d);
+	cr_assert_stdout_eq_str(result);
+}
+
+Test(test_printf_precision_octal, octal_minus_5_width_MAX_asterisk_precision_l_d, .init = redirect_std_out_octal_prec)
+{
+	long d;
+	char *result = NULL;
+	d = 9223372036854775807;
+	ft_printf("%-5.*lo", 32, d);
+	fflush(stdout);
+
+	asprintf(&result, "%-5.*lo", 32, d);
+	cr_assert_stdout_eq_str(result);
+}
+
+Test(test_printf_precision_octal, octal_minus_5_width_42_asterisk_precision_l_d, .init = redirect_std_out_octal_prec)
+{
+	long d;
+	char *result = NULL;
+	d = 42;
+	ft_printf("%-5.*lo", -5, d);
+	fflush(stdout);
+
+	asprintf(&result, "%-5.*lo", -5, d);
+	cr_assert_stdout_eq_str(result);
+}
+
+Test(test_printf_precision_octal, octal_minus_5_width_0_asterisk_precision_l_d, .init = redirect_std_out_octal_prec)
+{
+	long d;
+	char *result = NULL;
+	d = 0;
+	ft_printf("%-5.*lo", 0, d);
+	fflush(stdout);
+
+	asprintf(&result, "%-5.*lo", 0, d);
+	cr_assert_stdout_eq_str(result);
+}

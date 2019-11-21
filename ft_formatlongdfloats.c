41bba84619ed7ec291cb3d4d95c2081722ef5534
Imre van Teylingen <ivan-tey@student.codam.nl>
2019-11-21 22:26:05 +0100

    better start on floats

--- ft_formatlongdfloats.c
+++ ft_formatlongdfloats.c
@@ -6,7 +6,7 @@
 /*   By: ivan-tey <ivan-tey@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/21 13:08:28 by ivan-tey       #+#    #+#                */
-/*   Updated: 2019/11/21 17:25:20 by ivan-tey      ########   odam.nl         */
+/*   Updated: 2019/11/21 22:25:37 by ivan-tey      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,29 @@ int			ft_formatlongdfloats(t_info *info, float f)
 {
 	int		n;
 	char	*nb;
+	size_t	prec;
 
 	n = (int)f;
-	if (n < 0)
-		info->sign = -1;
+	prec = 0;
 	nb = ft_itoa_base(n, 10);
-	info->len = ft_strlen(nb);
 	info->writer(info->target, nb, 0);
 	if (info->precfound == 1 && info->precision == 0)
 		return (0);
 	info->writer(info->target, ".", 1);
-	while (f > 0)
+	if (n < 0)
+	{
+		info->sign = -1;
+		f = f * -1;
+		n = n * -1;
+	}
+	while (f > 0 && prec < info->precision)
 	{
 		f = f - n;
 		f = f * 10;
 		n = (int)f;
 		nb = ft_itoa(n);
-		info->writer(info->target, nb, 1);
+		info->writer(info->target, nb, 0);
+		prec++;
 	}
 	return (0);
 }
